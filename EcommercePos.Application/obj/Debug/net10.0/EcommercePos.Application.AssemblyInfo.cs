66c5a819eb053af9ab8e2d50a2a23d4c1b9b2d59
Md. Atikur Zaman <kaptan.cse@gmail.com>
2026-04-05 06:26:29 +0600

    Fix package references and build

--- EcommercePos.Application/obj/Debug/net10.0/EcommercePos.Application.AssemblyInfo.cs
+++ EcommercePos.Application/obj/Debug/net10.0/EcommercePos.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommercePos.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c372c61b2852562e9028942cc0b402f3e05b90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54ef0553ade15c60bb231d54f7ec852c1083d8fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommercePos.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommercePos.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
